bf8b2cf171cc9076470bcbdd4e3550d2c1740806
Vinayak Jamadar <vinayaksjamadar1234@gmail.com>
2022-08-01 18:47:18 +0530

    Resolved Admin Company Result Email Sending Error

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -93,57 +93,63 @@ module.exports.rounds_result = async (req, res) => {
             //     success: true,
             //     message: `Email send to ${qualStudents} successfully`,
             // });
+            if(qualMessage.length !=0){
 
-            await transporter.sendMail({
-                from: process.env.SMTP_SERVICE,
-                to: qualStudents,
-                subject: 'Qualification of Rounds',
-                html: qualMessage
-            }, (error, data) => {
-                if (error) {
-                    console.log(error);
-                    res.status(500).json({ message: 'ERROR SENDING MAIL !!!' });
-                } else {
-                    console.log("Sent! ", data.response, " messageId: ", data.messageId);
-                    res.status(200).json({ message: 'NOTIFICATION MAIL SENT !!!' });
-                }
-            });
+                await transporter.sendMail({
+                    from: process.env.SMTP_SERVICE,
+                    to: qualStudents,
+                    subject: 'Qualification of Rounds',
+                    html: qualMessage
+                }, async (error, data) => {
+                    if (error) {
+                        console.log(error);
+                        res.status(500).json({ message: 'ERROR SENDING MAIL !!!' });
+                    } else {
+                        // console.log("Sent! ", data.response, " messageId: ", data.messageId);
+                        // res.status(200).json({ message: 'NOTIFICATION MAIL SENT !!!' });
+                        if(disqualStudents.length !=0){
+
+                            await transporter.sendMail({
+                                from: process.env.SMTP_SERVICE,
+                                to: disqualStudents,
+                                subject: 'Qualification of Rounds',
+                                html: disqualMessage
+                            }, (error, data) => {
+                                if (error) {
+                                    console.log(error);
+                                    res.status(500).json({ message: 'ERROR SENDING MAIL !!!' });
+                                } else {
+                                    console.log("Sent!", data.response, " messageId: ", data.messageId);
+                                    res.status(200).json({ message: 'NOTIFICATION MAIL SENT !!!' });
+                                }
+                            });
+                        }
+                    }
+                });
+        }
             // console.log("Message sent: %s", info.messageId);
         } catch (err) {
             console.log('err in rounds_result', err);
         }
 
-        try {
-            // let info = await transporter.sendMail({
-            //     from: process.env.SMTP_SERVICE,
-            //     to: disqualStudents,
-            //     subject: 'Qualification of Rounds',
-            //     html: disqualMessage
-            // });
-            // res.status(200).json({
-            //     success: true,
-            //     message: `Email send to ${disqualStudents} successfully`,
-            // });
-            await transporter.sendMail({
-                from: process.env.SMTP_SERVICE,
-                to: disqualStudents,
-                subject: 'Qualification of Rounds',
-                html: disqualMessage
-            }, (error, data) => {
-                if (error) {
-                    console.log(error);
-                    res.status(500).json({ message: 'ERROR SENDING MAIL !!!' });
-                } else {
-                    console.log("Sent!", data.response, " messageId: ", data.messageId);
-                    res.status(200).json({ message: 'NOTIFICATION MAIL SENT !!!' });
-                }
-            });
-            // console.log("Message sent: %s", info.messageId);
-        } catch (err) {
-            console.log('err in rounds_result', err);
-        }
+        // try {
+        //     // let info = await transporter.sendMail({
+        //     //     from: process.env.SMTP_SERVICE,
+        //     //     to: disqualStudents,
+        //     //     subject: 'Qualification of Rounds',
+        //     //     html: disqualMessage
+        //     // });
+        //     // res.status(200).json({
+        //     //     success: true,
+        //     //     message: `Email send to ${disqualStudents} successfully`,
+        //     // });
 
-        res.status(200).json({ success: true, message: "Result Updated Successfully" });
+        //     // console.log("Message sent: %s", info.messageId);
+        // } catch (err) {
+        //     console.log('err in rounds_result', err);
+        // }
+
+        // res.status(200).json({ success: true, message: "Result Updated Successfully" });
     }
     catch {
         res.status(400).json({ success: false, message: "Error while updating the company details." });
