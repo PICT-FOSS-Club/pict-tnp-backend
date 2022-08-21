52fdd5d57b55dd04ee5a4a316a6b89069fcab2da
Rohit Patil <rohitvpatil0810@gmail.com>
2022-08-21 16:05:07 +0530

    Authentication Modified

--- middlewares/authAdmin.js
+++ middlewares/authAdmin.js
@@ -1,14 +1,26 @@
 const jwt = require("jsonwebtoken");
-
+const Admin = require("../models/admin");
 const authAdmin = async (req, res, next) => {
-    try {
-        const token = req.cookies.token;
-        const admin = await jwt.verify(token, process.env.JWT_SECRET);
+  const token = await req.cookies.token;
+  console.log(token);
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
+      if (err) {
+        let AuthError = {
+          success: false,
+          message: "1Admin is not authenticated!",
+        };
+        res.status(401).send({ AuthError });
+      } else {
+        const admin = await Admin.findById(decodedToken.id);
         req.admin = admin;
         next();
-    } catch {
-        res.status(401).send( { message: "Please Authenticate." } );
-    }
-}
+      }
+    });
+  } else {
+    let AuthError = { success: false, message: "2Admin is not authenticated!" };
+    res.status(401).json({ AuthError });
+  }
+};
 
 module.exports = authAdmin;
